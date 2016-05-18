c05d41e7ccbdf35df66d17aead1cc910004ac848
Felix Borzik <borzik1992@gmail.com>
2016-05-18 20:59:28 +0300

    fix ordering dd if order is nil

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,6 +1,6 @@
 module IdeasHelper
   def order_dropdown
-    dropdown ORDER_MAPPING.fetch(params[:order].to_sym){ 'Newest first' } do
+    dropdown ORDER_MAPPING.fetch(params[:order].try(:to_sym)){ 'Newest first' } do
       link_to('Newest first', ideas_path(filter: params[:filter], order: :newest)) +
           link_to('Oldest first', ideas_path(filter: params[:filter], order: :oldest))
     end
