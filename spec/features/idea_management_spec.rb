91317e818a763c5f43c601e357b899f42fea402c
Felix Borzik <borzik1992@gmail.com>
2016-05-22 17:06:24 +0300

    idea management spec

---
+++ spec/features/idea_management_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+describe 'idea creation' do
+  let(:user){ create :user }
+  before { login_as user }
+  it 'shows idea form' do
+    visit new_idea_path
+    expect(page).to have_text('I\'ve got an idea!')
+    expect(page).to have_field 'Title'
+    expect(page).to have_field 'Summary'
+    expect(page).to have_field 'Description'
+  end
+  it 'accepts idea submission' do
+    visit new_idea_path
+    idea = build :idea
+    fill_in 'Title', with: idea.title
+    fill_in 'Summary', with: idea.summary
+    fill_in 'Description', with: idea.description
+    expect{
+      click_button 'Create Idea'
+    }.to change(Idea, :count).by 1
+    expect(page).to have_text idea.title
+    expect(page).to have_text 'Idea was successfully created'
+  end
+end
+
+describe "idea editing" do
+  let(:user){ create :user }
+  before { login_as user }
+  let!(:idea){ create :idea, user: user }
+  it 'shows idea form' do
+    visit user_ideas_path(user)
+    expect(page).to have_text idea.title
+  end
+  it 'accepts idea submission' do
+    visit edit_idea_path(idea)
+    fill_in 'Title', with: 'New Idea Title'
+    fill_in 'Summary', with: 'New Idea Summary'
+    fill_in 'Description', with: 'New Idea Description'
+    expect{
+      click_button 'Update Idea'
+    }.to change{idea.reload.title}.from(idea.title).to('New Idea Title')
+    expect(page).to have_text 'New Idea Title'
+    expect(page).to have_text 'Idea was successfully updated'
+  end
+end
+
+describe "idea removal" do
+  let(:user){ create :user }
+  before { login_as user }
+  let!(:idea){ create :idea, user: user }
+  it 'deletes idea' do
+    visit user_ideas_path(user)
+    within("#idea_#{idea.id}") do
+      expect(page).to have_text idea.title
+      expect{
+        click_link 'Delete'
+      }.to change(Idea, :count).by(-1)
+    end
+  end
+end
--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,4 +1,8 @@
 require 'devise'
 RSpec.configure do |config|
-  config.include Devise::TestHelpers, :type => :controller
+  config.include Devise::TestHelpers, type: :controller
+  config.include Warden::Test::Helpers, type: :feature
+  config.before :suite do
+    Warden.test_mode!
+  end
 end
