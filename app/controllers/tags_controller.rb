d7264d76a3f66813737861d4156f18fab467992b
Felix Borzik <borzik1992@gmail.com>
2016-09-03 18:29:18 +0300

    sanitize tags query

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
   def index
     @tags = ActsAsTaggableOn::Tag.most_used(10)
-    @tags = @tags.where("name LIKE '#{ params[:q] }%'") if params[:q].present?
+    @tags = @tags.where("name LIKE ?", "#{ params[:q] }%") if params[:q].present?
     render json: { tags: @tags.pluck(:name) }
   end
 end
