7927e4ad123c787ec247d6f9773e7f79fac1f37b
Felix Borzik <borzik1992@gmail.com>
2016-12-11 14:41:20 +0200

    deploy database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :forward_agent, true     # SSH forward_agent.
 
 # shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # set :shared_dirs, fetch(:shared_dirs, []).push('somedir')
-set :shared_files, fetch(:shared_files, []).push('config/database.yml', '.env')
+set :shared_files, fetch(:shared_files, []).push('.env')
 
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
