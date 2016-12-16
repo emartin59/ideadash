0ea756a1ff9ef590baf9d9b1a51700a5fa73e099
Felix Borzik <borzik1992@gmail.com>
2016-12-16 16:15:12 +0200

    fix algolia without app id

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,12 +37,7 @@ set :shared_files, fetch(:shared_files, []).push('.env')
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
 task :environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
-  # invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use', 'ruby-1.9.3-p125@default'
+#   invoke :'rvm:use', 'ruby-2.3.1@ideadash'
 end
 
 # Put any custom commands you need to run at setup
--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,4 +1,4 @@
 AlgoliaSearch.configuration = {
     application_id: ENV['ALGOLIASEARCH_APPLICATION_ID'],
     api_key: ENV['ALGOLIASEARCH_API_KEY']
-}
+} if ENV['ALGOLIASEARCH_APPLICATION_ID'].present?
